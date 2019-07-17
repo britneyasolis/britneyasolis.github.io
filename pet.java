2fd84021e4741c6314fc79ebd69fe454d908509e
CNA Marketing <cnamarketing@Britney.local>
2019-07-17 10:27:07 -0500

    added my website files

---
+++ pet.java
@@ -0,0 +1,19 @@
+<button onclick="myFunction()">Click me</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "
+    Address: 1616 Deerfield Rd, Highland Park, IL 60035
+    Hours:
+    Monday	Closed
+    Tuesday	12pm to 6pm
+    Wednesday	12pm to 6pm
+    Thursday	11am to7pm
+    Friday	11am to 7pm
+    Saturday	11am to 5pm
+    Sunday	11am to 4pm
+    Phone: (224) 707-1190";
+}
+</script>
